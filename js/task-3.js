1f5686a3730d8fffd0ff26282f05078e135ff550
Vadym1288 <141283561+Vadym1288@users.noreply.github.com>
2024-01-23 21:48:58 +0200

    goit-js-hw-03-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,14 @@
-function getElementWidth(content, padding, border) {
-    var result = parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-    return result;
-}
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px"));
+function filterArray(numbers, value) {
+    let array = [];
+    for (let i of numbers) {
+      if (i > value) {
+        array.push(i);
+      }
+    }
+    return array;
+  }
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20)); 
